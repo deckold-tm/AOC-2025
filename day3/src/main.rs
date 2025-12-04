74c860a818a0c97ee675edca9ada62752f05a805
David Eckold <david@eckold.dev>
2025-12-04 09:53:58 +0000

    update

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -5,19 +5,24 @@ mod tests {
         "987654321111111\n811111111111119\n234234234234278\n818181911112111\n";
     #[test]
     fn test_part1() {
+        // env_logger::init();
+        assert_eq!(part1(TEST_INPUT), Ok(357))
+    }
+    #[test]
+    fn test_part2() {
         env_logger::init();
-        assert_eq!(part1(TEST_INPUT).unwrap(), 357)
+        assert_eq!(part2(TEST_INPUT), Ok(3121910778619))
     }
 }
-
-use std::fmt::Debug;
-use std::fs::read_to_string;
 
 use nom::character::complete::{digit1, line_ending};
 use nom::combinator::map_res;
 use nom::error::Error;
 use nom::{Finish, IResult, Parser};
 use nom::{multi::many1, sequence::terminated};
+use std::fmt::Debug;
+use std::fs::read_to_string;
+use std::iter::Sum;
 
 fn digits(input: &str) -> IResult<&str, Vec<u32>> {
     let digits = terminated(digit1, line_ending);
@@ -32,38 +37,74 @@ fn digits(input: &str) -> IResult<&str, Vec<u32>> {
 fn parse_input(input: &str) -> Result<(&str, Vec<Vec<u32>>), Error<&str>> {
     many1(digits).parse(input).finish()
 }
+fn battery_check1(battery: &Vec<u32>) -> usize {
+    let (idx, a) = battery[..battery.len() - 1]
+        .iter()
+        .enumerate()
+        .fold((0, 0), |cur, (idx, a)| -> (usize, u32) {
+            if *a > cur.1 { (idx, *a) } else { cur }
+        });
+    // log::debug!("{:?}\n{:?}, {:?}", battery, idx, a);
+    let b = battery[idx + 1..].iter().max().unwrap();
+    let prod = format!("{}{}", a, b);
+    // log::debug!("jolts: {:?}", prod);
+    prod.parse().expect("Parsing back into usize failed")
+}
+fn battery_check2(battery: &Vec<u32>) -> usize {
+    const MAX: usize = 12;
+    let mut vals: Vec<u32> = Vec::new();
+    let len = battery.len();
+    let mut ii = 0;
+    log::debug!("current battery: {:?}", battery);
+    for n in 1..=MAX {
+        let max = len - MAX;
+        log::debug!("current battery slice: {:?}", battery[ii..max + n].to_vec());
+        let (idx, a) = battery[ii..max + n]
+            .iter()
+            .enumerate()
+            .fold((0, 0), |cur, (idx, a)| -> (usize, u32) {
+                if *a > cur.1 { (idx, *a) } else { cur }
+            });
+        vals.push(a);
+        ii += idx + 1;
+    }
+    let prod = vals
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join("");
+    log::debug!("jolts: {:?}", prod);
+    prod.parse().expect("Parsing back into usize failed")
+}
 
-fn part1(input: &str) -> Option<usize> {
+fn sum_jolts<T>(input: &str, battery_check: fn(&Vec<u32>) -> T) -> Result<T, Error<&str>>
+where
+    T: Sum,
+{
     let parsed_input = parse_input(input);
     match parsed_input {
         Ok((_, input)) => {
             log::debug!("{:?}", input);
-            let jolts: u32 = input
-                .into_iter()
-                .map(|battery: Vec<u32>| {
-                    let (idx, a) = battery[..battery.len() - 1].iter().enumerate().fold(
-                        (0, 0),
-                        |cur, (idx, a)| -> (usize, u32) {
-                            if *a > cur.1 { (idx, *a) } else { cur }
-                        },
-                    );
-                    log::debug!("{:?}\n{:?}, {:?}", battery, idx, a);
-                    let b = battery[idx + 1..].iter().max().unwrap();
-                    let prod = format!("{}{}", a, b);
-
-                    log::debug!("jolts: {:?}", prod);
-                    prod.parse().unwrap()
-                })
-                .collect::<Vec<u32>>()
+            let jolts = input
                 .iter()
+                .map(battery_check)
+                .collect::<Vec<T>>()
+                .into_iter()
                 .sum();
-            Some(jolts as usize)
+            Ok(jolts)
         }
         Err(err) => {
             log::error!("Unable to parse input with error: {:?}", err);
-            None
+            Err(err)
         }
     }
+}
+fn part1(input: &str) -> Result<usize, Error<&str>> {
+    sum_jolts(input, battery_check1)
+}
+fn part2(input: &str) -> Result<usize, Error<&str>> {
+    sum_jolts(input, battery_check2)
+    // todo!()
 }
 
 fn print_answer<T: Debug>(part: usize, ans: T) {
@@ -73,7 +114,14 @@ fn main() {
     env_logger::init();
     let input = read_to_string("./input.txt");
     match input {
-        Ok(input) => print_answer(1, part1(&input)),
+        Ok(input) => {
+            if let Ok(ans1) = part1(&input) {
+                print_answer(1, ans1)
+            }
+            if let Ok(ans2) = part2(&input) {
+                print_answer(2, ans2)
+            }
+        }
         Err(err) => log::error!("Failed to read input with error {:?}", err),
     }
 }
